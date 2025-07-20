578209e00d4c349f859ddb69b467a173d5afcb14
Gerardo Vazquez <neowhatup@gmail.com>
2025-07-20 11:30:05 -0600

    20

---
+++ condicionales.py
@@ -0,0 +1,13 @@
+
+nota = 80
+
+if nota >= 90:
+    print('aprobaste la materia')
+    print('felicidades exelente calificacion')
+elif nota >= 80:
+    print('aprobaste la materia')
+    print('felicidades Buenas calificaciones')
+elif nota >= 60:
+    print('aprobaste la materia')
+else:
+    print('Reprobaste la materia')
--- trasporte_publico.py
+++ trasporte_publico.py
@@ -4,3 +4,31 @@
 # - 5-18 o mayores de 60: 50% descuento
 # - Resto: tarifa completa
 
+pasaje = int(input('ingresa tu edad: '))
+tarifa_completa = 10
+descuento = 0
+"""
+if pasaje < 5:
+    print('pasaje Gratis')
+elif (pasaje >= 5 and pasaje <= 18) or (pasaje >= 60):
+    print('Tienes  el 50% de descuento')
+else:
+    print('tarifa completa')
+ """
+if pasaje < 5:
+    descuento = 0
+    print('Tu pasaje es Gratis')
+
+elif pasaje >= 5 and pasaje <= 18:
+    descuento = tarifa_completa * 0.50
+    print('Tienes el 50% de Descuento')
+
+elif pasaje >= 60:
+    descuento = tarifa_completa * 0.50
+    print('Tienes el 50% de Descuento')
+
+else:
+    descuento = tarifa_completa
+    print('pagas tarifa completa')
+
+print(f'pagas ${descuento:.2f}')
