bb8d02d2845f0af5f1e088696b2136dfd1722cfb
Gerardo Vazquez <neowhatup@gmail.com>
2025-07-20 12:11:41 -0600

    20

--- condicionales.py
+++ condicionales.py
@@ -1,4 +1,4 @@
-
+"""
 nota = 80
 
 if nota >= 90:
@@ -11,3 +11,13 @@ elif nota >= 60:
     print('aprobaste la materia')
 else:
     print('Reprobaste la materia')
+"""
+
+frutas_favoritas = ['manzana', 'platano','mango', 'zandia']
+
+fruta = input('Ingresa tu fruta Favorita: ')
+
+if fruta in frutas_favoritas:
+    print(f'{fruta} es una fruta delisiosa')
+else:
+    print(f'{fruta} no es una fruta delisiosa')
