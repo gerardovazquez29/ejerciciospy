91fc5cf2cb70c99e3249e63a007769dcde7e7e07
Gerardo Vazquez <neowhatup@gmail.com>
2025-07-22 18:47:21 -0600

    22

---
+++ ejercicio0.py
@@ -0,0 +1,39 @@
+edad =10
+
+if edad >= 10 and edad <= 30:
+    print('Edad dentro del rango permitido')
+else:
+    print('tu edad no es permitida')
+
+usuario = True
+admin = True
+
+if usuario and admin:
+    print('Tiene acceso total')
+else:
+    print('Acceso limitado')
+
+# Verifica si un número NO está dentro del rango permitido
+numero = 150
+if not (numero >= 1 and numero <= 100):
+    print("Número fuera de rango")
+
+# Validar entrada de un cliente a un sistema
+tiene_credencial = True
+es_empleado = False
+autorizado = True
+
+if (tiene_credencial and autorizado) or es_empleado:
+    print("Puede entrar al sistema")
+else:
+    print("Acceso denegado")
+    
+# Combinación lógica compleja
+edad = 17
+permiso_padres = True
+acompanado_por_adulto = False
+
+if edad >= 18 or (permiso_padres and acompanado_por_adulto):
+    print("Entrada permitida")
+else:
+    print("Entrada denegada")
--- ejercicio1.py
+++ ejercicio1.py
@@ -1,35 +1,78 @@
 
-def calcular_maximo(n1,n2,n3):
-   
-    if n1 >= n2 and n1 >= n3:
-        mayor = n1
-    elif n2 >= n1 and n2 >= n3:
-        mayor = n2
-    else:
-        mayor = n3
-    return mayor
-n1= int(input("Ingrese el primer número: "))
-n2= int(input("Ingrese el segundo número: "))   
-n3= int(input("Ingrese el tercer número: "))
-maximo = calcular_maximo(n1, n2, n3)
-print(f"El número mayor es: {maximo}")
+# Verifica si un número es positivo y par.
+num = 5
+if num % 2 == 0 and num >= 0:
+    print(f'El numero: {num} es positivo y es par')
+elif num % 2 == 0 or num >= 0:
+    print(f'El numero: {num} no es par y es positivo')
+else:
+    print(f'El numero: {num} no es positivo ni es par')
 
+# Comprobar si un usuario ingresó correctamente el usuario o la contraseña.
+usuario = 'gerardo1'
+contraseña = 1234
+usuario_almacenado = 'gerardo'
+contraseña_almacenada = 1234  
 
-def operaciones(*numeros):
-    suma = 0
-    resta = 0
-    multiplicacion = 1
-    division = 1
-    for numero in numeros:
-        suma += numero
-        resta -= numero
-        multiplicacion *= numero
-        if numero != 0:
-            division /= numero
+if contraseña == contraseña_almacenada and usuario == usuario_almacenado:
+    print('acseso consedido')
+else:
+    print('acceso denegado')
 
-    return suma, resta, multiplicacion, division
-sum,rest,mult,div = operaciones(5,3,2,9,10)
-print(f"Suma: {sum}")
-print(f"Resta: {rest}")
-print(f"Multiplicación: {mult}")
-print(f"División: {div}")
+# Verifica si una persona puede votar (edad >= 18) y tiene INE.
+persona = 18
+tiene_ine = False
+if persona >= 18 and tiene_ine:
+    print('Puede votar')
+else:
+    print('No puede votar')
+
+# Determina si una persona puede entrar a una fiesta: 
+# si es mayor de edad o está acompañado por un adulto.
+persona = 16
+acompañado_adulto = True
+if persona >= 18:
+    print('Puede entrar ala fiesta sin restricciones')
+elif persona < 18 and acompañado_adulto:
+    print('Puede entrar con adulto pero no puede consumir alcohol')
+else:
+    print('No puede entrar ala fiesta ')
+
+# Verifica si un número NO está entre 100 y 200.
+numero = 50
+if numero >= 100 and numero <= 200:
+    print('El numero esta entre el 100 y el 200')
+else:
+    print('El numero no esta entre el 100 y el 200')
+
+# Validar si un usuario no está bloqueado y su cuenta está activa.
+
+usuario_bloqueado = False
+cuenta_activa = False
+if not usuario_bloqueado and cuenta_activa:
+    print('Tiene acceso')
+elif not usuario_bloqueado or cuenta_activa:
+    print('No tiene acceso  su cuenta esta bloqueada')
+else:
+    print('No tiene acceso el usuario esta bloqueado')
+
+# Verifica si un número es divisible 
+# por 3 o por 5 pero no ambos (usa lógica exclusiva)
+numero = 10
+  
+if (numero % 3 == 0) and (numero % 5 != 0):
+    print('El numero es divisible entre 3 pero no de 5')
+elif (numero % 5 == 0) and (numero % 3 != 0):
+    print('El numero es divisible entre 5 pero no de 3')
+else:
+    print('el numero es divisible por ambos o por ninguno')
+
+# Validar si una cadena de texto no está vacía 
+# y tiene más de 5 caracteres
+texto = 'cade'
+longitud = len(texto)
+if longitud > 0 and longitud > 5:
+    print('la cadena de texto no esta vacia y tiene mas de 5 caracteres')
+else:
+    print('No cumple con lo requerido')
+
